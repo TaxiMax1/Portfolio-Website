7679e3b66e8e44055c600c6affc3e2e3cd9a3ad9
TaxiMax1 <taximaxi1808@gmail.com>
2025-01-29 19:47:41 +0100

    Home Page Update
    made a menu for phone

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,13 @@
 function toggleNav() {
     const nav = document.getElementById('navbar-container');
+    const menuIcon = document.getElementById('menu-icon');
+  
     nav.classList.toggle('active');
+  
+    if (nav.classList.contains('active')) {
+      menuIcon.innerHTML = '&times;'; 
+    } else {
+      menuIcon.innerHTML = '&#9776;'; 
+    }
   }
+  
